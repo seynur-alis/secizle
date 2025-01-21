ae02e44bd2888e11719cd42e1ed68603db1f5d35
seynur-alis <seynuralis@hotmail.com>
2025-01-21 19:25:16 +0300

    Update tmdb.ts

--- apps/web/src/services/tmdb.ts
+++ apps/web/src/services/tmdb.ts
@@ -1,4 +1,4 @@
 import { TMDB } from '@plotwist_app/tmdb'
 
-export const tmdb = TMDB(process.env.NEXT_PUBLIC_TMDB_API_KEY || '')
+export const tmdb = TMDB('1ce059b6141a8f30c43c11841d8cffbc')
 export * from '@plotwist_app/tmdb'
